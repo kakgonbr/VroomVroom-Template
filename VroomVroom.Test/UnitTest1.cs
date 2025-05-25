11436326bd3b659419aad2f670c6ae69f0fdbc6e
kakgonbri <buikhacbin@gmail.com>
2025-05-25 23:09:58 +0700

    Add project files.

---
+++ VroomVroom.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+
+namespace VroomVroom.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            VroomVroom.Server.Program.doSomething().Should().BeFalse();
+        }
+    }
+}
